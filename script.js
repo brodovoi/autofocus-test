85fb615fa0e8ff20343dcd58d86b37b62430b173
Artem Borodovoi <artem_borodovoio@tchdm.live>
2023-05-12 19:59:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const initCreateAccountEmail = () => {
+    // VARS
+    const form = document.querySelector('[data-sign-form]');
+    if (!form) return;
+    const inputEmail = form.querySelector('[data-validate-field-up]');
+
+    // Автоматический фокус
+    inputEmail.focus();
+    
+    // Задержка для обеспечения срабатывания фокуса
+    setTimeout(() => {
+        inputEmail.focus();
+      
+        // Проверка на мобильные устройства
+        if (isMobileDevice()) {
+            inputEmail.click();
+        }
+    }, 500);
+};
+
+// Проверка на мобильные устройства
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+initCreateAccountEmail();
