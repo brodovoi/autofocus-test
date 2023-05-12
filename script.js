d88e5bdb436f04fb8e6a25f2e15e21fd6204da81
Artem Borodovoi <artem_borodovoio@tchdm.live>
2023-05-12 20:30:43 +0300

    updated script

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Проверка на мобильные устройства
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
 const initCreateAccountEmail = () => {
     // VARS
     const form = document.querySelector('[data-sign-form]');
@@ -6,23 +11,18 @@ const initCreateAccountEmail = () => {
 
     // Автоматический фокус
     inputEmail.focus();
-    
-    // Задержка для обеспечения срабатывания фокуса
-    setTimeout(() => {
-        inputEmail.focus();
-      
-        // Проверка на мобильные устройства
-        if (isMobileDevice()) {
-            inputEmail.click();
-        }
-    }, 500);
+
+    // Проверка на мобильные устройства
+    if (isMobileDevice() && /iPad|iPhone|iPod/.test(navigator.userAgent)) {
+        // Создание виртуального события для открытия клавиатуры
+        const event = new Event('click', {
+            bubbles: true,
+            cancelable: true,
+            view: window
+        });
+        inputEmail.dispatchEvent(event);
+    }
 };
-
-// Проверка на мобильные устройства
-function isMobileDevice() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-}
-
 
 document.addEventListener('DOMContentLoaded', function() {
     // Ваш код инициализации здесь
